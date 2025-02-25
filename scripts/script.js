5103d24a836e84188e6e1f074dcba2b113635e35
noa-cen <noacengarle@gmail.com>
2025-02-25 19:53:28 +0100

    updates themes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,23 @@
-// Fonction menu drop up
-function dropup() {
-    document.getElementById("dropup").classList.toggle("show")
+// Fonction menu drop up games
+function dropupGames() {
+    document.getElementById("dropup-games").classList.toggle("show")
 }
-let dropupBtn = document.getElementById("dropup-btn")
-dropupBtn.addEventListener("click", dropup)
+let dropupBtnGames = document.getElementById("dropup-btn-games")
+dropupBtnGames.addEventListener("click", dropupGames)
+
+// Fonction menu drop up colors
+function dropupColors() {
+    document.getElementById("dropup-colors").classList.toggle("show")
+}
+let dropupBtnColors = document.getElementById("dropup-btn-colors")
+dropupBtnColors.addEventListener("click", dropupColors)
+
+// Change themes
+let colorTogglers = document.querySelectorAll(".fa-brush")
+function changeTheme(event) {
+    let color = event.currentTarget.id
+    document.body.classList.toggle(color)
+}
+colorTogglers.forEach((toggle) => {
+    toggle.addEventListener("click", changeTheme)
+})
