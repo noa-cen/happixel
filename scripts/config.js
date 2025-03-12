b14c7b57c2a8ef2b95006e136ab2896988e5181c
noa-cen <noacengarle@gmail.com>
2025-03-12 18:07:05 +0100

    hangman

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,4 @@
 // Tictactoe
-
 let gameMode = ""
 let currentPlayer = ""
 let player1 = "X"
@@ -30,3 +29,41 @@ let turns = parseInt(localStorage.getItem("turns")) || 0
 
 let nextGameBtn = document.getElementById("nextGame")
 let restartBtn = document.getElementById("restart")
+
+// Hangman
+let wordList = [
+    "Beau", "Bête", "Boxe", "Brun", "Cerf", "Chez", "Cire", "Dame", "Dent", "Dock", "Dodo", "Drap", "Dune", 
+    "Emeu", "Fado", "Faux", "Ibis", "Jazz", "Joli", "Joue", "Kaki", "Logo", "Loin", "Long", "Lune", "Lynx", 
+    "Mine", "Mûre", "Ouïe", "Ours", "Pion", "Rhum", "Ride", "Rock", "Seau", "Test", "Thym", "Trou", "Truc", 
+    "User", "Vert", "Yogi", "Watt", "Accès", "Aimer", "Aloès", "Assez", "Avion", "Balai", "Banjo", "Barbe", 
+    "Bonne", "Bruit", "Buche", "Cache", "Capot", "Carte", "Chien", "Crâne", "Cycle", "Ébène", "Essai", 
+    "Gifle", "Honni", "Jambe", "Koala", "Livre", "Lourd", "Moult", "Noeud", "Ortie", "Pêche", "Poire", 
+    "Pomme", "Poste", "Prune", "Radar", "Radis", "Robot", "Route", "Rugby", "Seuil", "Taupe", "Tenue", 
+    "Texte", "Tyran", "Usuel", "Valse", "Acajou", "Agneau", "Alarme", "Ananas", "Angora", "Animal", 
+    "Arcade", "Aviron", "Azimut", "Babine", "Balade", "Bonzaï", "Basson", "Billet", "Bouche", "Boucle", 
+    "Bronze", "Cabane", "Caïman", "Cloche", "Chèque", "Cirage", "Coccyx", "Crayon", "Garage", "Gospel", 
+    "Goulot", "Gramme", "Grelot", "Hochet", "Hormis", "Humour", "Hurler", "Jargon", "Limite", "Lionne", 
+    "Menthe", "Oiseau", "Podium", "Poulpe", "Poumon", "Puzzle", "Quartz", "Rapide", "Séisme", "Tétine", 
+    "Tomate", "Walabi", "Whisky", "Abriter", "Ballast", "Baryton", "Bassine", "Batavia", "Billard", 
+    "Bretzel", "Cithare", "Chariot", "Clairon", "Corbeau", "Cortège", "Crapaud", "Cymbale", "Dentier", 
+    "Djembé", "Drapeau", "Exemple", "Fourmis", "Grandir", "Iceberg", "Javelot", "Jockey", "Journal", 
+    "Journée", "Jouxter", "Losange", "Macadam", "Mondial", "Notable", "Oxygène", "Panique", "Pétrole", 
+    "Poterie", "Pouvoir", "Renégat", "Scooter", "Senteur", "Sifflet", "Spirale", "Sucette", "Strophe", 
+    "Tonneau", "Trousse", "Tunique", "Ukulélé", "Vautour", "Zozoter", "Aquarium", "Araignée", "Arbalète", 
+    "Archipel", "Banquise", "Batterie", "Brocante", "Brouhaha", "Capeline", "Clavecin", "Cloporte", "Débutant", 
+    "Diapason", "Gangster", "Gothique", "Hautbois", "Hérisson", "Logiciel", "Objectif", "Paranoïa", "Parcours", 
+    "Pastiche", "Question", "Quetsche", "Scarabée", "Scorpion", "Symptôme", "Tabouret", "Toujours", "Tourisme", 
+    "Triangle", "Utopique", "Zeppelin", "Accordéon", "Ascenseur", "Ascension", "Aseptiser", "Autoroute", 
+    "Avalanche", "Bilboquet", "Bourricot", "Brillance", "Cabriolet", "Contrario", "Cornemuse", "Dangereux", 
+    "Épluchage", "Féodalité", "Forteresse", "Gondolier", "Graphique", "Horoscope", "Intrépide", "Klaxonner", 
+    "Mascarade", "Métaphore", "Narrateur", "Péripétie", "Populaire", "Printemps", "Quémander", "Tambourin", 
+    "Vestiaire", "Xylophone" ];
+
+let hangmanLifes = document.querySelectorAll(".lifes");
+let wordToGuess = document.querySelector(".wordToGuess");
+let word;
+let nbLifes = 0;
+let letters = document.querySelectorAll(".letter");
+letters.forEach(letter => {
+    letter.addEventListener("click", checkLetter);
+});
--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,51 +1,69 @@
-let hangmanLifes = document.querySelector(".hangmanLifes");
-let wordToGuess = document.querySelector(".wordToGuess");
-
-let wordList = [
-    "Beau", "Bête", "Boxe", "Brun", "Cerf", "Chez", "Cire", "Dame", "Dent", "Dock", "Dodo", "Drap", "Dune", 
-    "Emeu", "Fado", "Faux", "Ibis", "Jazz", "Joli", "Joue", "Kaki", "Logo", "Loin", "Long", "Lune", "Lynx", 
-    "Mine", "Mûre", "Ouïe", "Ours", "Pion", "Rhum", "Ride", "Rock", "Seau", "Test", "Thym", "Trou", "Truc", 
-    "User", "Vert", "Yogi", "Watt", "Accès", "Aimer", "Aloès", "Assez", "Avion", "Balai", "Banjo", "Barbe", 
-    "Bonne", "Bruit", "Buche", "Cache", "Capot", "Carte", "Chien", "Crâne", "Cycle", "Ébène", "Essai", 
-    "Gifle", "Honni", "Jambe", "Koala", "Livre", "Lourd", "Moult", "Noeud", "Ortie", "Pêche", "Poire", 
-    "Pomme", "Poste", "Prune", "Radar", "Radis", "Robot", "Route", "Rugby", "Seuil", "Taupe", "Tenue", 
-    "Texte", "Tyran", "Usuel", "Valse", "Acajou", "Agneau", "Alarme", "Ananas", "Angora", "Animal", 
-    "Arcade", "Aviron", "Azimut", "Babine", "Balade", "Bonzaï", "Basson", "Billet", "Bouche", "Boucle", 
-    "Bronze", "Cabane", "Caïman", "Cloche", "Chèque", "Cirage", "Coccyx", "Crayon", "Garage", "Gospel", 
-    "Goulot", "Gramme", "Grelot", "Hochet", "Hormis", "Humour", "Hurler", "Jargon", "Limite", "Lionne", 
-    "Menthe", "Oiseau", "Podium", "Poulpe", "Poumon", "Puzzle", "Quartz", "Rapide", "Séisme", "Tétine", 
-    "Tomate", "Walabi", "Whisky", "Abriter", "Ballast", "Baryton", "Bassine", "Batavia", "Billard", 
-    "Bretzel", "Cithare", "Chariot", "Clairon", "Corbeau", "Cortège", "Crapaud", "Cymbale", "Dentier", 
-    "Djembé", "Drapeau", "Exemple", "Fourmis", "Grandir", "Iceberg", "Javelot", "Jockey", "Journal", 
-    "Journée", "Jouxter", "Losange", "Macadam", "Mondial", "Notable", "Oxygène", "Panique", "Pétrole", 
-    "Poterie", "Pouvoir", "Renégat", "Scooter", "Senteur", "Sifflet", "Spirale", "Sucette", "Strophe", 
-    "Tonneau", "Trousse", "Tunique", "Ukulélé", "Vautour", "Zozoter", "Aquarium", "Araignée", "Arbalète", 
-    "Archipel", "Banquise", "Batterie", "Brocante", "Brouhaha", "Capeline", "Clavecin", "Cloporte", "Débutant", 
-    "Diapason", "Gangster", "Gothique", "Hautbois", "Hérisson", "Logiciel", "Objectif", "Paranoïa", "Parcours", 
-    "Pastiche", "Question", "Quetsche", "Scarabée", "Scorpion", "Symptôme", "Tabouret", "Toujours", "Tourisme", 
-    "Triangle", "Utopique", "Zeppelin", "Accordéon", "Ascenseur", "Ascension", "Aseptiser", "Autoroute", 
-    "Avalanche", "Bilboquet", "Bourricot", "Brillance", "Cabriolet", "Contrario", "Cornemuse", "Dangereux", 
-    "Épluchage", "Féodalité", "Forteresse", "Gondolier", "Graphique", "Horoscope", "Intrépide", "Klaxonner", 
-    "Mascarade", "Métaphore", "Narrateur", "Péripétie", "Populaire", "Printemps", "Quémander", "Tambourin", 
-    "Vestiaire", "Xylophone" ];
-
 function chooseWord() {
     let i = Math.floor(Math.random() * wordList.length);
-    let word = wordList[i];
-    //////////////////////////////////////////////////////////////////
-    console.log(word);
+    word = wordList[i];
+
     wordToGuess.innerHTML = word.charAt(0);
+
     for (let x = 1; x < word.length; x++) {
         wordToGuess.innerHTML += " _";
     }
 }
-chooseWord()
 
-function guessLetter() {
-    let letter = document.querySelector(".letter");
+function checkLetter(event) {
+    let id = event.target.id;
 
-    if (word.includes(letter)) {
-        let position = word.indexOf(letter);
-        wordToGuess.innerHTML = word.charAt(position);
+    event.target.classList.add("disabled");
+
+    if (word.toLowerCase().includes(id)) {
+        let displayedWord = wordToGuess.innerHTML.split(" ");
+        for (let i = 0; i < word.length; i++) {
+            if (word[i].toLowerCase() === id) {
+                displayedWord[i] = word[i];
+            }
+        }
+        wordToGuess.innerHTML = displayedWord.join(" ");
+
+        if (!wordToGuess.innerHTML.includes("_")) {
+            setTimeout(() => alert("Bravo ! Vous avez gagné !"), 100);
+            restart();
+            return;
+        }
+    }
+    else {
+        if (nbLifes < hangmanLifes.length - 1) {
+            hangmanLifes[nbLifes].classList.add("none");
+            nbLifes++;
+            hangmanLifes[nbLifes].classList.remove("none");
+        }
+        if (nbLifes === hangmanLifes.length - 1) {
+            setTimeout(() => alert(`Dommage ! Le mot était "${word}".`), 100);
+            restart();
+            return;
+        }      
     }
 }
+
+function restart() {
+    let restart = document.getElementById("restart")
+    restart.classList.remove("none")
+    restart.classList.add("block")
+    restart.addEventListener("click", () => {
+        location.reload()
+    })
+}
+
+function playHangman() {
+    nbLifes = 0;
+
+    hangmanLifes.forEach((img, index) => {
+        if (index === 0) {
+            img.classList.remove("none"); 
+        } else {
+            img.classList.add("none"); 
+        }
+    });
+
+    chooseWord();
+}
+
+playHangman();
